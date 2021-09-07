462183e206b4ac5c21087ecd7d2f99a5313b9c89
Maxim Efremov <maximefremov@mail.ru>
2021-09-07 20:10:22 +0300

    Очистка JS от лишнего кода.

--- src/js/modules/overlay.js
+++ src/js/modules/overlay.js
@@ -21,19 +21,6 @@ class Overlay {
     });
   }
 
-  _getScrollbarWidth() {
-    const outer = document.createElement('div');
-    outer.style.visibility = 'hidden';
-    outer.style.overflow = 'scroll';
-    outer.style.msOverflowStyle = 'scrollbar';
-    document.body.appendChild(outer);
-    const inner = document.createElement('div');
-    outer.appendChild(inner);
-    const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
-    outer.parentNode.removeChild(outer);
-    return scrollbarWidth;
-  }
-
   toggle() {
     this.overlayEl.classList.toggle(this.overlayActiveClass);
     this.bodyEl.classList.toggle('fixed');
--- src/js/modules/scrollTo.js
+++ src/js/modules/scrollTo.js
@@ -17,8 +17,6 @@ class ScrollTo {
         top: y - self.offset,
         behavior: 'smooth'
       });
-
-
     }))
   }
 
